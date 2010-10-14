c03ccbb75f9c1d284f001887c9776cd1ea0bfa39
Ricardo <ricardo.wurmus@gmail.com>
2010-10-14 19:25:36 +0800

    64 note polyphony

--- mdaEPiano.h
+++ mdaEPiano.h
@@ -8,7 +8,7 @@
 #define NPARAMS 12       //number of parameters
 #define NPROGS   8       //number of programs
 #define NOUTS    2       //number of outputs
-#define NVOICES 32       //max polyphony
+#define NVOICES 64       //max polyphony
 #define SUSTAIN 128
 #define SILENCE 0.0001f  //voice choking
 #define WAVELEN 422414   //wave data bytes
