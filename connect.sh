5bda5b34a39e35fb54d73f905bc3de824eb140ee
Ricardo <ricardo.wurmus@gmail.com>
2010-10-17 21:17:30 +0800

    no 'known issues' anymore

--- connect.sh
+++ connect.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#jack_connect mdaEPiano:midi alsa_pcm:MIDIMATE-II/midi_capture_1
-
+# connect to JACK
 jack_connect mdaEPiano:midi "a2j:Midi Through [14] (capture): Midi Through Port-0"
-jack_connect system:playback_1 mdaEPiano:left                  
+jack_connect system:playback_1 mdaEPiano:left
 jack_connect system:playback_2 mdaEPiano:right
