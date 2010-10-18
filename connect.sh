684551404bcc772d917b938049c90891a4854dc0
Ricardo <ricardo.wurmus@gmail.com>
2010-10-18 23:35:12 +0800

    ALSA MIDI connect
    Included the connection from my MIDI interface to the Midi Through port
    via aconnect. This will not be useful for most people, but I needed it
    to get a testing environment quicker.

--- connect.sh
+++ connect.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+# connect MIDI mate II to MIDI through (Alsa MIDI)
+aconnect 28:0 14:0
+
 # connect to JACK
 jack_connect mdaEPiano:midi "a2j:Midi Through [14] (capture): Midi Through Port-0"
+
 jack_connect system:playback_1 mdaEPiano:left
 jack_connect system:playback_2 mdaEPiano:right
