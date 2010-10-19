d620c0d1df7304fcd423c5490541f1d411a6d07b
Ricardo <ricardo.wurmus@gmail.com>
2010-10-19 08:18:52 +0800

    Added parameters as control ports
    will have to modify the plugin to actually care about the control ports
    and not the mess of internal variables.
    
    MIDI controllers should affect the control ports, which in turn can be
    read and changed from within the GUI.

--- mdaEPianoCommon.h
+++ mdaEPianoCommon.h
@@ -10,18 +10,18 @@
 //#define WAVELEN 422414   //wave data bytes
 
 enum Parameter {
-	envelope_decay_param,	/* 0*/
-	envelope_release_param,	/* 1*/
-	hardness_param,			/* 2*/
-	treble_boost_param,		/* 3*/
-	mod_param,				/* 4*/
-	lfo_rate_param,			/* 5*/
-	velosense_param,		/* 6*/
-	stereo_width_param,		/* 7*/
-	polyphony_param,		/* 8*/
-	fine_tuning_param,		/* 9*/
-	random_tuning_param,	/*10*/
-	overdrive_param			/*11*/
+	envelope_decay_param,	/* 0: 0.500*/
+	envelope_release_param,	/* 1: 0.500*/
+	hardness_param,			/* 2: 0.500*/
+	treble_boost_param,		/* 3: 0.500*/
+	mod_param,				/* 4: 0.500*/
+	lfo_rate_param,			/* 5: 0.650*/
+	velosense_param,		/* 6: 0.250*/
+	stereo_width_param,		/* 7: 0.500*/
+	polyphony_param,		/* 8: 0.50*/
+	fine_tuning_param,		/* 9: 0.500*/
+	random_tuning_param,	/*10: 0.146*/
+	overdrive_param			/*11: 0.000*/
 };
 
 static float scale_midi_to_f(unsigned char data)
