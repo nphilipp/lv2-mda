ffc26866298acdc95261b27ca86c1cb729f5fd53
Ricardo <ricardo.wurmus@gmail.com>
2012-01-16 23:00:18 +0800

    clean up

--- src/mdaEPianoCommon.h
+++ src/mdaEPianoCommon.h
@@ -6,22 +6,19 @@
 #define NVOICES 64       //max polyphony
 #define SUSTAIN 128
 #define SILENCE 0.0001f  //voice choking
-#define CONTROL_PORT_OFFSET 3 //number of non-control ports
 
 struct KGRP  //keygroup
 {
-	long root;  //MIDI root note
-	long high;  //highest note
-	long pos;
-	long end;
-	long loop;
+  long root;  //MIDI root note
+  long high;  //highest note
+  long pos;
+  long end;
+  long loop;
 };
 
 static float scale_midi_to_f(unsigned char data)
 {
-	float scaled;
-	scaled = 0.0078f * (float)(data);
-	return scaled;
+  return 0.0078f * (float)(data);
 }
 
 #endif
